612ed5d546b419be88b8364798c348776113a650
alexander-winkler <alexander_winkler@posteo.de>
2022-03-30 08:23:22 +0200

    Wed 30 Mar 08:23:22 CEST 2022

--- workflow.sh
+++ workflow.sh
@@ -1,1 +1,1 @@
-python3.9 degruyter_feedgenerator.py && git add . && git commit -m "$(date)" && git push
+cd ~/Documents/degruyter_rss && python3.9 degruyter_feedgenerator.py && git add . && git commit -m "$(date)" && git push
